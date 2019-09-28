f4a66405d6677435b08cd1eeb39c9e282ffd5812
Bryant Pham <bryantphamm@gmail.com>
2019-09-28 00:12:05 -0500

    Assignment4/Fix opaque rendering

--- Assignment4/js/Assignment_4.1.js
+++ Assignment4/js/Assignment_4.1.js
@@ -538,6 +538,13 @@ function cleanScene() {
  * Draw the scene
  */
 function drawScene() {
+    gl.clearColor(0.3, 0.3, 0.3, 1.0);  // Clear to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+
+    // Clear the canvas before we start drawing on it.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     const projectionMatrix = mat4.create();
 
     if (cameraMode == CameraModes.PERSPECTIVE) {
